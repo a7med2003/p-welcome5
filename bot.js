e0528d2e290ee19c8212387ea71fbb5754fed774
a7med2003 <54036248+a7med2003@users.noreply.github.com>
2019-08-12 20:49:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,20 +4,25 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+var prefix = "-";
 
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose: ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
 
+client.on('guildMemberAdd', member => {///كود الترحيب في السيرفر
+  
+  const channel = member.guild.channels.find(ch => ch.name === 'general');///اسم الشات
+ 
+  if (!channel) return;
 
-
-
-
-// CODES HERE
-
-
-
-
-
-
+  channel.send(`**welcome:heart:️**${member}`);
+})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
